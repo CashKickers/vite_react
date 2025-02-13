48dc69f1cab3cc35d6e2edf2e1973922c6f50bc7
ljhyeon <gee9328@gmail.com>
2025-02-13 23:53:45 +0900

    feat: Update modal component design for enhanced user experience

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,44 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import PropTypes from 'prop-types'
 import { Button, Image } from 'antd-mobile'
 
 import MyButton from './MyButton'
+// 그래프 모듈 임포트
 
 import '../styles/global.css'
 import '../styles/modal.css'
 
-import modalClose from '../assets/modal-close.svg'
-import address from '../assets/address.svg'
+import modalCloseIcon from '../assets/modal-close.svg'
+import addressIcon from '../assets/address.svg'
+import flavorIcon from '../assets/flavor.svg'
+import priceIcon from '../assets/price.svg'
+import cleanIcon from '../assets/clean.svg'
+import customerIcon from '../assets/customer.svg'
+import moodIcon from '../assets/mood.svg'
 
 const Modal = ( { isOpen, onClose, id, } ) => {
+    const navigate = useNavigate();
+
+    // 식당 기본 정보
+    const [name, setName] = useState();
+    const [isMy, setIsMy] = useState(false);
+    const [address, setAddress] = useState();
+    // 리뷰 변화
+    const [month, setMonth] = useState(0);
+    const [changeStatus, setChangeStatus] = useState();
+    // 리뷰 요약
+    const reviewSumIcons = [
+        {type: 'flavor', icon: flavorIcon},
+        {type: 'price', icon: priceIcon},
+        {type: 'clean', icon: cleanIcon},
+        {type: 'customer', icon: customerIcon},
+        {type: 'mood', icon: moodIcon}
+    ];
+    const [reviewSums, setReviewSums] = useState([{ type: '', icon: '', content: ''}]);
+    // 이미지
+    const [imageLinks, setImageLinks] = useState([]);
 
     return (
         <>
@@ -21,13 +50,13 @@ const Modal = ( { isOpen, onClose, id, } ) => {
                         <div className="modal-header-info">
                             <div className="modal-header-name">
                                 <div style={{paddingRight: "7px"}}>광화문 한옥집</div>
-                                <MyButton />
+                                <MyButton setState={isMy} />
                             </div>
-                            <Image src={modalClose} width="50px" onClick={onClose} />
+                            <Image src={modalCloseIcon} width="50px" onClick={onClose} />
                         </div>
                         <div className="modal-header-address">
-                            <Image src={address} width="15px" />
-                            <span style={{paddingLeft: "5px"}}>서울특별시 종로구 새문안로5가길 7 세종클럽 지하 1층</span>
+                            <Image src={addressIcon} width="13px" />
+                            <span style={{paddingLeft: "2px"}}>서울특별시 종로구 새문안로5가길 7 세종클럽 지하 1층</span>
                         </div>
                     </div>
 
@@ -36,59 +65,50 @@ const Modal = ( { isOpen, onClose, id, } ) => {
                         <div className="modal-content">
                                 {/* 1번 멘트 */}
                                 <div>
-                                    이 식당은 <span className="bold">5개월 간</span>
+                                    <span className="bold">5개월 간</span>의 리뷰가
                                 </div>
                                 {/* 그래프 */}
                                 <div>
-                                    그래프 들어갈 공간
+                                    그래프 ~~ !!
                                 </div>
                                 {/* 2번 멘트 */}
                                 <div>
-                                    평점이 <span className="bold">꾸준히 오르고</span> 있어요!
+                                    점점 <span className="bold">긍정적</span>으로 변하고 있어요!
                                 </div>
                         </div>
 
                         {/* 요약 정보 */}
+                        <div className='modal-header2'>리뷰 요약</div>
+                        <div className='modal-info'>* 생성형 AI가 요약한 리뷰입니다.</div>
                         <div className="modal-content">
-                            <div className='modal-header2'>리뷰 요약</div>
-                            <div className='modal-review-sum'>
-                                <Image src={address} width={15} />
-                                <span>
-                                    맛에 대한 평가가 대체적으로 좋아요
-                                </span>
-                            </div>
-                            <div className='modal-review-sum'>
-                                <Image src={address} width={15} />
-                                <span>
-                                    맛에 대한 평가가 대체적으로 좋아요
-                                </span>
-                            </div>
-                            <div className='modal-review-sum'>
-                                <Image src={address} width={15} />
-                                <span>
-                                    맛에 대한 평가가 대체적으로 좋아요
-                                </span>
-                            </div>
-                            <span>* 생성형 AI가 요약한 리뷰입니다.</span>
+                            {reviewSums.map((type, icon, content) => {
+                                <div className='modal-review-sum' key={type}>
+                                    <Image src={icon} width={15} />
+                                    <span>
+                                        {content}
+                                    </span>
+                                </div>
+                            })}
                         </div>
 
-                        <div className="modal-content">
-                            <div className="modal-header2">대표 이미지</div>
-                            {/* 이미지 */}
-                            <div className="modal-image">
-                                <div className="modal-image-1">
-                                    <Image src={address} />
-                                </div>
-                                <div className="modal-image-2">
-                                    <Image src={address} />
-                                    <Image src={address} />
-                                </div>
+                        <div className="modal-header2">대표 이미지</div>
+                        {/* <div className="modal-content"> */}
+                        {/* 이미지 */}
+                        <div className="modal-image">
+                            <div className="modal-image-1">
+                                <Image src={imageLinks[0]} fit='fill' />
+                            </div>
+                            <div className="modal-image-2">
+                                <Image src={imageLinks[1]} fit='contain' />
+                                <Image src={imageLinks[2]} fit='contain' />
                             </div>
                         </div>
+                        {/* </div> */}
                     </div>
 
                     <div className="modal-footer">
                         <Button
+                            block
                             shape='rounded'
                             style={{ backgroundColor: 'var(--yellow)', color: '#fff' }} 
                         >
