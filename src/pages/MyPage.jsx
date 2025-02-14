47ec73bbdf8bd8fdc84130b899cbdadc1fbb68ee
ljhyeon <gee9328@gmail.com>
2025-02-15 08:49:25 +0900

    feat: Add my page api

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'
 import { Image } from 'antd-mobile'
 
 import MyButton from '../components/MyButton';
+
+import { restaurantApi } from '../api/restaurant'
 
 import moreIcon from '../assets/more.svg'
 
@@ -13,6 +15,59 @@ const MyPage = () => {
 
   // localstorage에 저장된 값 불러와서 처리하기
   const [myList, setMyList] = useState([]);
+
+  // 식당 기본 정보
+  const [restaurants, setRestaurants] = useState([]); // [{id: , name: , address: , image: ,}, ...]
+
+  // api
+  // - 식당 정보
+  const loadRestaurant = async ( id ) => {
+    try {
+      const data = await restaurantApi({ id }); // Call the function with the correct parameters
+      console.log("restaurant: ", data);
+
+      if (data && id === data.id) {
+        setRestaurants(prev => [
+          ...prev, {
+            id: data.id,
+            name: data.name,
+            address: data.address,
+            image: data.images[0] || null,
+          }
+        ])
+      }
+    } catch (error) {
+      console.error('API 호출 중 오류 발생:', error);
+    }
+  };
+
+  // 로컬 스토리지에 있는 값 들고오기
+  useEffect(() => {
+    const myInLocal = localStorage.getItem('my')
+    console.log(myInLocal)
+    if (myInLocal) {
+      try {
+        const parsedList = JSON.parse(myInLocal);
+        setMyList(parsedList);
+      } catch (error) {
+        console.error('Error parsing JSON from localStorage:', error);
+      }
+    }
+
+    return () => {
+      setMyList([])
+      setRestaurants([])
+    }
+  }, [])
+
+  useEffect(()=> {
+    console.log(myList)
+    if (myList.length > 0) {
+      myList.forEach(my => {
+        loadRestaurant(my);
+      });
+    }
+  }, [myList])
 
   // 상세 페이지 이동
   const onClickMore = ( id ) => {
@@ -27,41 +82,44 @@ const MyPage = () => {
       padding: "20px 15px",
     }}>
       {/* myList 반복할 것 */}
-      <div className="my-content">
-        <Image 
-          src={"https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8JTIzaW1hZ2V8ZW58MHx8MHx8fDA%3D"}
-          width={65}
-          height={65}
-          fit='cover'
-          style ={{
-            flex: 1.3,
-            borderRadius: 8
-          }}
-        />
-        <div className="my-content-info">
-          <div className="my-content-name">
-            이름
+      {restaurants.map((restaurant) => (
+        <div className="my-content" key={restaurant.id}>
+          <Image 
+            src={restaurant.image}
+            width={65}
+            height={65}
+            fit='cover'
+            style ={{
+              flex: 1.3,
+              borderRadius: 8
+            }}
+          />
+          <div className="my-content-info">
+            <div className="my-content-name">
+            {restaurant.name}
+            </div>
+            <div className="my-content-address">
+            {restaurant.address}
+            </div>
           </div>
-          <div className="my-content-address">
-            주소
+          {/* 버튼: 상세 페이지로 이동 */}
+          <div
+            style ={{
+              flex: 1,
+            }}
+            onClick = {() => onClickMore(1)} // item.key 등처럼 상황에 맞게 변경
+          >
+            <Image src={moreIcon} height={15} />
           </div>
+          <MyButton
+            setState='true'
+            id = {restaurant.id}
+            style = {{
+              flex: 1,
+            }}
+          />
         </div>
-        {/* 버튼: 상세 페이지로 이동 */}
-        <div
-          style ={{
-            flex: 1,
-          }}
-          onClick = {() => onClickMore(1)} // item.key 등처럼 상황에 맞게 변경
-        >
-          <Image src={moreIcon} height={15} />
-        </div>
-        <MyButton
-          setState='true'
-          style = {{
-            flex: 1,
-          }}
-        />
-      </div>
+      ))}
     </div>
   )
 }
