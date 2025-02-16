2dda9ba2a88825cc632873ec97ed7fe0c9cb28b6
ljhyeon <gee9328@gmail.com>
2025-02-16 14:00:30 +0900

    feat: add Graph data api

---
+++ src/api/graphYM.js
@@ -0,0 +1,25 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+export const graphYM = async ( {id} ) => {
+    const url = `${BASE_URL}/graph/ym/${id}`
+    try {
+        const response = await fetch( url, { method: 'GET', } )
+        if (!response.ok) {
+            if (response.status === 404) {
+                console.log('No Graph Data');
+                return [];
+            }
+            else {
+                throw new Error('Network response was not ok ' + response.statusText);
+            }
+        }
+
+        const data = await response.json();
+
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+        throw error;
+    }
+}
